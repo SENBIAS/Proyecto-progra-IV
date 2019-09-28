e608c5b288fee6784b04cd8766c9015e4a7c7c0f
SENBIAS <49045371+SENBIAS@users.noreply.github.com>
2019-09-28 13:59:27 -0500

    Revisar el main. En Labores tengo dudas, falta ya muy poco para acabarlo

--- src/main/java/CRUD/crudProductor.java
+++ src/main/java/CRUD/crudProductor.java
@@ -46,7 +46,7 @@ public class crudProductor {
             }else
             {
                 productorEncontrado.imprimir();
-                //¡¡¡MOSTRAR VIVEROS!!!
+                CrudVivero.mostrarViveros(productorEncontrado.getViveros());
             }
         }
     }
